e2307acbc1a6806ec72766bc688739ca7aca7211
Cristian Gonzalez <cristian.gonzalez-r@mail.escuelaing.edu.co>
2026-06-08 21:09:53 -0500

    Feat:Complete task 6.4

---
+++ ejercicio6.4/ChatService.java
@@ -0,0 +1,10 @@
+package chatrmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ChatService extends Remote {
+
+    void recibirMensaje(String mensaje) throws RemoteException;
+
+}
